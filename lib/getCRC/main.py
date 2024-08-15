4c7305a61632c3183df7fc7dcb10f98de25bf1dc
adiherlambang <adiherl91@gmail.com>
2024-08-15 16:05:25 +0700

    tshoot

--- lib/getCRC/main.py
+++ lib/getCRC/main.py
@@ -133,7 +133,7 @@ def proc_iface_crc_xr(device,counter):
 def proc_iface_crc_nx(device,counter):
     try:
         logger.info("Pyats parser with nxos type function")
-        device.connect(learn_hostname = True, learn_os = True, log_stdout=True, mit=True, timeout=5000)
+        device.connect(learn_hostname = True, learn_os = True, log_stdout=True, mit=True, command_timeout=500)
         logger.info(f"Device: {device.name}")
         output_iface_crc = device.parse('show interface')
         check=['port-channel','mgmt0','loopback','Vlan','.']
